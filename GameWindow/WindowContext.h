85a1db3ab3eced93d619c8f8e43cf8de1fbecfdc
bboczula <boczula.bartosz@gmail.com>
2020-11-21 20:47:37 +0100

    Removed default constructor from the WindowContext

--- GameWindow/WindowContext.h
+++ GameWindow/WindowContext.h
@@ -7,10 +7,8 @@ namespace Astral
 {
 	struct WindowContext
 	{
-		WindowContext() : hwnd(nullptr), instance(nullptr), width(1280), height(720)
-		{
-		}
-		WindowContext(HWND hwnd, HINSTANCE instance, UINT width, UINT height) : hwnd(hwnd), instance(instance), width(width), height(height)
+		WindowContext(HWND hwnd, HINSTANCE instance, UINT width, UINT height)
+			: hwnd(hwnd), instance(instance), width(width), height(height)
 		{
 		}
 		HWND hwnd;
